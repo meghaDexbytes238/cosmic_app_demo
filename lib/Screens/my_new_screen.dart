a0a3a3244ca9b07832398d7bb1ed71fc5adc9e46
ay-dex <megha.dexbytes@gmail.com>
2024-04-04 16:12:24 +0530

    new screen add

---
+++ lib/Screens/my_new_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class My_new_Screen extends StatefulWidget {
+  const My_new_Screen({super.key});
+
+  @override
+  State<My_new_Screen> createState() => _My_new_ScreenState();
+}
+
+class _My_new_ScreenState extends State<My_new_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
