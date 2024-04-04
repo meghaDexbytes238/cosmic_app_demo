e5887715fa744db833e2b6f83c19ca2b3c86a521
ay-dex <megha.dexbytes@gmail.com>
2024-04-04 17:48:34 +0530

    add textfield in style

--- lib/Screens/my_new_screen.dart
+++ lib/Screens/my_new_screen.dart
@@ -12,7 +12,7 @@ class _My_new_ScreenState extends State<My_new_Screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Text('megha'),
+      body: Text('megha'  , textAlign: TextAlign.center),
     );
   }
 }
